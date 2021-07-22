cb0422a90ec344d4f3ac90f7ad5e29e8735ba529
Happy Singh <happy@mtatva.com>
2021-07-22 06:30:25 +0530

    Unbounded knapsack done

---
+++ dp/unbounded_knapsack_variations/coin_change_min_num_of_coins.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int t[n+1][amount+1];
+        
+        for(int i=0; i<n+1; ++i){
+            for(int j=0; j<amount+1; ++j){
+                if(j == 0) {
+                    t[i][j] = 0;
+                }
+                if(i == 0) {
+                    t[i][j] = INT_MAX - 1;
+                }
+            }
+        }
+        for(int i = 1; i<amount+1; ++i){
+            if(i%coins[0] == 0)
+                t[1][i] = i/coins[0];
+            else 
+                t[1][i] = INT_MAX - 1;
+        }
+                
+        for(int i=2; i<n+1; ++i){
+            for(int j=1; j<amount+1; ++j){
+                if( coins[i-1] <= j )
+                    t[i][j] = min( 1 + t[i][j-coins[i-1]], t[i-1][j] );
+                else 
+                    t[i][j] = t[i-1][j];
+            }
+        }
+        
+        return t[n][amount] > amount ? -1 : t[n][amount];
+        
+    }
+};
